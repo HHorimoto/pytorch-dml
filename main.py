a2673620ebb2f972a8e878a76c294a2c4c693152
HHorimoto <81685394+HHorimoto@users.noreply.github.com>
2025-03-26 12:26:46 +0900

    add: learning method

--- main.py
+++ main.py
@@ -22,17 +22,22 @@ def main():
     EPOCHS = config_file['config']['epochs']
     BATCH_SIZE = config_file['config']['batch_size']
     LR = config_file['config']['learning_rate']
+    METHOD = config_file['config']['method']
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     
     train_loader, test_loader = create_dataset(root=ROOT, download=True, batch_size=BATCH_SIZE)
 
-    model_1, model_2 = CNN(widen_factor=1).to(device), CNN(widen_factor=1).to(device)
+    model_1, model_2 = CNN(widen_factor=2).to(device), CNN(widen_factor=2).to(device)
     optimizer_1, optimizer_2 = optim.Adam(model_1.parameters(), lr=LR), optim.Adam(model_2.parameters(), lr=LR)
     loss_ce, loss_kl = nn.CrossEntropyLoss(), kl_divergence
 
-    coach = CoachDML([model_1, model_2], train_loader, test_loader, 
+    if METHOD == 'dml':
+        coach = CoachDML([model_1, model_2], train_loader, test_loader, 
                      [loss_ce, loss_kl], [optimizer_1, optimizer_2], device, EPOCHS)
+    else:
+        coach = Coach(model_1, train_loader, test_loader, loss_ce, optimizer_1, device, EPOCHS)
+        
     coach.train_test()
 
     print("accuracy: ", coach.test_acc[-1])
