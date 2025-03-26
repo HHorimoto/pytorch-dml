916d2f42bc17f5d034b82d40bf803e4bda646334
HHorimoto <81685394+HHorimoto@users.noreply.github.com>
2025-03-26 12:20:45 +0900

    update: model

--- src/models/models.py
+++ src/models/models.py
@@ -12,16 +12,12 @@ class CNN(nn.Module):
         self.fc3 = nn.Linear(1024*widen_factor, 10)
         self.act = nn.ReLU()
         self.pool = nn.MaxPool2d(2, 2)
-        self.dropout = nn.Dropout(0.5)
     
     def forward(self, x):
         x = self.pool(self.act(self.conv1(x)))
         x = self.pool(self.act(self.conv2(x)))
         x = x.view(x.size()[0], -1)
-        x = self.dropout(x)
         x = self.act(self.fc1(x))
-        x = self.dropout(x)
         x = self.act(self.fc2(x))
-        x = self.dropout(x)
         x = self.fc3(x)
         return x
